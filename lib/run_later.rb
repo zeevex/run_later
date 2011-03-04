a84b754caeba4119841c1d4bd2a179df56a1a338
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-03-04 08:11:36 -0500

    [ARCH] Fixed bugs with Rails 3 loading
    - Use ActiveSupport.on_load hooks so run_later plays nice with everyone else.

--- lib/run_later.rb
+++ lib/run_later.rb
@@ -11,12 +11,25 @@ module RunLater
   end
 end
 
-ActionController::Base.send(:include, RunLater::InstanceMethods) if defined?(ActionController)
+if defined?(Rails) && Rails::VERSION::MAJOR > 2
+  ActiveSupport.on_load :action_controller do
+    ActionController::Base.send(:include, RunLater::InstanceMethods) 
+  end
 
-# Make run_later available both as instance and class methods
-if defined?(ActiveRecord)
-  ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
-  ActiveRecord::Base.extend(RunLater::InstanceMethods)
-  ActiveRecord::Observer.send(:include, RunLater::InstanceMethods)
-  ActiveRecord::Observer.extend(RunLater::InstanceMethods)
+  ActiveSupport.on_load :active_record do
+    ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
+    ActiveRecord::Base.extend(RunLater::InstanceMethods)
+    ActiveRecord::Observer.send(:include, RunLater::InstanceMethods)
+    ActiveRecord::Observer.extend(RunLater::InstanceMethods)
+  end
+else
+  ActionController::Base.send(:include, RunLater::InstanceMethods) if defined?(ActionController)
+
+  # Make run_later available both as instance and class methods
+  if defined?(ActiveRecord)
+    ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
+    ActiveRecord::Base.extend(RunLater::InstanceMethods)
+    ActiveRecord::Observer.send(:include, RunLater::InstanceMethods)
+    ActiveRecord::Observer.extend(RunLater::InstanceMethods)
+  end
 end
